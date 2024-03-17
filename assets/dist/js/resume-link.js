9d0363eb4a78d0ac81d745ebb5b62fea8e7761ca
sidh8artha <sidh8bansode@yahoo.com>
2024-03-17 11:04:38 +0530

    new update

---
+++ assets/dist/js/resume-link.js
@@ -0,0 +1,8 @@
+// Get a reference to the anchor element
+var link = document.getElementById('myResume');
+
+// Your URL variable
+var myURL = 'https://www.google.com';
+
+// Update the href attribute of the anchor element
+link.href = myURL;
