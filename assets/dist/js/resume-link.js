90fdae73298af2345fab38d0a365f318ba34ab9b
sidh8artha <sidh8bansode@yahoo.com>
2024-03-17 11:29:30 +0530

    corrections.

--- assets/dist/js/resume-link.js
+++ assets/dist/js/resume-link.js
@@ -2,7 +2,7 @@
 var link = document.getElementById('myResume');
 
 // Your URL variable
-var myURL = 'https://www.google.com';
+var myURL = 'https://drive.google.com/file/d/1VWyJh-DMvSdzP5Of07E-GP636Vo5iBD7/view?usp=sharing';
 
 // Update the href attribute of the anchor element
 link.href = myURL;
