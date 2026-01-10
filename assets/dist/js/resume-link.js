3df08af1deaab7a87b51b51c4f58c3a277a68736
sidh8artha <sidh8bansode@yahoo.com>
2026-01-10 15:35:10 +0530

    2025 portfolio update

--- assets/dist/js/resume-link.js
+++ assets/dist/js/resume-link.js
@@ -2,7 +2,7 @@
 var link = document.getElementById('myResume');
 
 // Your URL variable
-var myURL = 'https://drive.google.com/file/d/1VWyJh-DMvSdzP5Of07E-GP636Vo5iBD7/view?usp=sharing';
+var myURL = 'https://drive.google.com/file/d/1mm-B2xcRGS7mONBV6omvn-6br99heIbr/view?usp=sharing';
 
 // Update the href attribute of the anchor element
 link.href = myURL;
