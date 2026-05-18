498137e561061b0504d523bacb9b6ba803dac2a6
sidh8artha <sidh8bansode@yahoo.com>
2026-05-18 23:49:11 +0530

    all images and cv link updated

--- assets/dist/js/resume-link.js
+++ assets/dist/js/resume-link.js
@@ -2,7 +2,8 @@
 var link = document.getElementById('myResume');
 
 // Your URL variable
-var myURL = 'https://drive.google.com/file/d/1mm-B2xcRGS7mONBV6omvn-6br99heIbr/view?usp=sharing';
+var myURL = 'https://drive.google.com/file/d/12GhMd1r-ddBIJu7dSbi6TyVoyohPlxzh/view?usp=sharing';
+
 
 // Update the href attribute of the anchor element
 link.href = myURL;
